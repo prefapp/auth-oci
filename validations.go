cc5aa0ad2e6a66ebddecfe72024b4fb609038388
Jose Alvarez <itasahobby@gmail.com>
2026-04-17 13:35:22 +0200

    fix: correct formatting in validation schema definition

--- validations.go
+++ validations.go
@@ -32,7 +32,7 @@ const SCHEMA = `{
 		"required": ["services", "charts"]
 	  }
 	},
-	"required": ["name", "registry", "image_types", "base_paths"],
+	"required": ["name", "registry", "image_types", "base_paths"]
   }`
 
 func validate() {
